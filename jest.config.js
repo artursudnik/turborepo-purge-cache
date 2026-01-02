c34042f3e662c0b037f93c89d9c7ea9c555f800d
Artur Sudnik-Hrynkiewicz <artur.sudnik@gmail.com>
2026-01-02 14:10:19 +0100

    test: configure jest to collect coverage and set thresholds

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,15 @@ module.exports = {
   testEnvironment: 'node',
   testMatch: ['<rootDir>/src/**/*.spec.ts'],
   passWithNoTests: true,
+  collectCoverageFrom: ['src/**/*.{ts,js}'],
+  coverageDirectory: './coverage',
+  coveragePathIgnorePatterns: ['index\\.ts'],
+  coverageThreshold: {
+    './src': {
+      branches: 70,
+      functions: 70,
+      lines: 70,
+      statements: 70,
+    },
+  },
 };
